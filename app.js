48658c66b21a2d029bd7494d7eb6af387f17168f
Marcel2 <marcel2@Marcels-MacBook-Pro.local>
2025-09-06 13:21:37 -0400

    added working gif search

--- app.js
+++ app.js
@@ -20,12 +20,34 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "AIzaSyCRWniVkeQMPyX_9-G4y9Nb7p8ZKoERPwk", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
 // Routes
-// example URL "http://localhost:3000/?term=hey"
 app.get('/', (req, res) => {
-    console.log(req.query) // => "{ term: hey" }[/bold]
-    res.render('home')
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
 })
+// example URL "http://localhost:3000/?term=hey"
+// app.get('/', (req, res) => {
+//     console.log(req.query) // => "{ term: hey" }[/bold]
+//     res.render('home')
+// })
 // app.get('/', (req, res) => {
 //     // set the url of the gif
 //     const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
